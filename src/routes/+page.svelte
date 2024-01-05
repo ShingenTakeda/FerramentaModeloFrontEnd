53f6d6fd1d1f561253c6244bc230583a7ea8efd4
ShingenTakeda <gustakeda@gmail.com>
2024-01-05 08:11:29 -0300

    MVP almost completed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,82 @@
 <script>
-    import { Input, Label, Helper, Button, Checkbox, A } from 'flowbite-svelte';
-    import { DarkMode } from 'flowbite-svelte';
+    import { Input, Label, Helper, Button } from 'flowbite-svelte';
+    import { DarkMode } from 'flowbite-svelte'; 
+    import 'js-file-download'
     import axios from 'axios';
+    import fileDownload from 'js-file-download';
 
-    async function test()
+    let dfd;
+    let dfd_copy;
+    
+    async function getDFD()
     {
         try
         {
-            const res = await axios.get('http://localhost:8000/JSON/test')
+            const res = await axios.get('http://localhost:8000/JSON/dfd')
+            //await console.log(res.data)
+            dfd = await res.data
+            dfd_copy = await dfd
         }
         catch(error)
         {
-            console.log("")
+            console.log("BAD ENDPOINT")
         }
     }
+
+    function resetForm()
+    {
+        dfd = dfd_copy
+    }
+
+    function debugDFD()
+    {
+        console.log(dfd)
+    }
+
+    async function send()
+    {
+        axios(
+        {
+            method:"POST",
+            url:"http://127.0.0.1:8000/docx", 
+            data: dfd,
+            responseType: 'blob'
+        })
+        .then(function (response)
+        {
+            fileDownload(response.data, 'dfd.docx');
+            //fileDownload
+        })
+        .catch(error => console.log("ERROR: " + error))
+    }
 </script>
+
+<DarkMode />
+{#await getDFD()}
+<h1 class="text-6xl dark:text-white">Fetching Stuff...</h1>
+{:then}
 <main>
-    <DarkMode />
+    <h1 class="text-6xl dark:text-white">Nome do Modelo: <br> {dfd[0]["t"]}</h1>
     <form>
+        {#each dfd as section}
+        {#if section["type"] == "p"}
+            
+            <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{section["ajuda"]}</label>
+            <textarea id="message" rows="4" 
+            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            placeholder="Write your thoughts here..." on:change={debugDFD} bind:value={section["c"][0]["t"]}></textarea>
+
+        {:else if section["type"] == "h"}
+            <Label for="first_name" class="mb-2">{section["ajuda"]}</Label>
+            <Input type="text" id="first_name" placeholder="Texto" required bind:value={section["c"][0]["t"]}/>
+        <!-- {:else if section["type"] == "t"}
+            <p>Test table</p> -->
+        {/if}
+        {/each}
+    </form>
+    <Button type="download" on:click={send}>Download</Button>
+    <Button type="reset" on:click={resetForm}>Reset</Button>
+    <!-- <form>
         <div class="grid gap-6 mb-6 md:grid-cols-2">
         <div>
             <Label for="first_name" class="mb-2">First name</Label>
@@ -61,5 +120,8 @@
         </Checkbox>
         <Button type="submit">Mandar</Button>
         <Button type="download">Download</Button>
-  </form>
+  </form> -->
 </main>
+{:catch error}
+<h1 class="text-6xl dark:text-white">{error.message}</h1>
+{/await}
